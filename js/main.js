fb8de568bc5076ba7a02b2ac9f90783eda909a49
Vlad Shamov <holovlad@yandex.ru>
2021-02-01 04:10:39 +1000

    Правки

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const getRandomNumber = function (min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const getRandomNumberWithFloat = function(min, max, floatSigns) {
+const getRandomNumberWithFloat = function (min, max, floatSigns) {
   return (Math.random() * (max - min + 1) + min).toFixed(floatSigns);
 }
 alert(getRandomNumber(0, 10.05));
