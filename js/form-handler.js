98152acd7e39f17ce9cd75126cb9d239b7a97f04
Vlad Shamov <holovlad@yandex.ru>
2021-02-22 00:55:02 +1000

    Правки

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,41 +1,42 @@
-const offerType = document.querySelector('#type')
-const priceInput = document.querySelector('#price')
-const checkinTime = document.querySelector('#timein')
-const checkoutTime = document.querySelector('#timeout')
-
 const bungalowMinPrice = '0'
 const flatMinPrice = '1000'
 const palaceMinPrice = '10000'
 const houseMinPrice = '5000'
 
-priceInput.placeholder =  flatMinPrice
+const offerType = document.querySelector('#type')
+const priceInput = document.querySelector('#price')
+const checkinTime = document.querySelector('#timein')
+const checkoutTime = document.querySelector('#timeout')
 
-offerType.addEventListener('change', (event) => {
-  if (event.target.value === 'palace'){
+priceInput.placeholder =  flatMinPrice
+priceInput.min = flatMinPrice
+
+offerType.addEventListener('change', (evt) => {
+  if (evt.target.value === 'palace'){
     priceInput.placeholder = palaceMinPrice
     priceInput.min = palaceMinPrice
   }
 
-  if (event.target.value === 'flat'){
+  if (evt.target.value === 'flat'){
     priceInput.placeholder = flatMinPrice
     priceInput.min = flatMinPrice
   }
 
-  if (event.target.value === 'bungalow'){
+  if (evt.target.value === 'bungalow'){
     priceInput.placeholder = bungalowMinPrice
     priceInput.min = bungalowMinPrice
   }
 
-  if (event.target.value === 'house'){
+  if (evt.target.value === 'house'){
     priceInput.placeholder = houseMinPrice
     priceInput.min = houseMinPrice
   }
 });
 
-checkinTime.addEventListener('change', (event) => {
-  checkoutTime.value = event.target.value
+checkinTime.addEventListener('change', (evt) => {
+  checkoutTime.value = evt.target.value
 });
 
-checkoutTime.addEventListener('change', (event) => {
-  checkinTime.value = event.target.value
+checkoutTime.addEventListener('change', (evt) => {
+  checkinTime.value = evt.target.value
 });
